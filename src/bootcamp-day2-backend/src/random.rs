6c25c20e52a976c5ff8e3255b6cde4ee002b039f
Tofixrs <tofix.rs@proton.me>
2024-06-26 21:27:54 +0200

    feat: better randomness

--- src/bootcamp-day2-backend/src/random.rs
+++ src/bootcamp-day2-backend/src/random.rs
@@ -1,10 +1,25 @@
+use std::cell::RefCell;
+
 use getrandom::Error;
+use oorandom::Rand64;
+
+thread_local! {
+    static RAND: RefCell<Rand64> = RefCell::new(Rand64::new(ic_cdk::api::time() as u128));
+}
 
 fn get_random(dest: &mut [u8]) -> Result<(), Error> {
-    let mut rand = oorandom::Rand64::new(ic_cdk::api::time() as u128);
-    for i in 1..=dest.len() {
-        dest[i - 1] = rand.rand_u64() as u8
-    }
+    let rand = RAND.with(|rand| {
+        let mut rand = rand.borrow_mut();
+        Rand64::new((rand.rand_u64() + ic_cdk::api::time()) as u128)
+    });
+    RAND.set(rand);
+
+    RAND.with(|rand| {
+        let mut rand = rand.borrow_mut();
+        for i in 1..=dest.len() {
+            dest[i - 1] = rand.rand_u64() as u8
+        }
+    });
     Ok(())
 }
 
